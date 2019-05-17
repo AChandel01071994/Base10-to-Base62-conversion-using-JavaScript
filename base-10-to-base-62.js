f0a3ad64f58ee5444e29a3f33e4b8c501060aff6
AChandel01071994 <31288986+AChandel01071994@users.noreply.github.com>
2019-05-17 17:38:26 +0530

    initial commit

---
+++ base-10-to-base-62.js
@@ -0,0 +1,29 @@
+{
+    function base10Tobase62(num){
+        let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let tiny = '';
+        while(num > 0){
+            tiny = chars[num % 62] + tiny;
+            num = Math.floor(num / 62);
+        }
+        return tiny;
+    }
+
+    function base62Tobase10(tiny){
+       let chars  = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let indices = [], original = 0;
+        // fetch indices (remainders) from the tiny string
+        for(let i = 0; i < chars.length; i++){
+            if(tiny.split('').includes(chars[i]))
+                indices.push(i);
+        }
+        // use indices to fetch original num
+        for(let i = 0; i < indices.length;i++){
+            original += indices[i] * Math.pow(62,i);
+        }
+
+        return original;
+    }
+   
+    base62Tobase10(base10Tobase62(125))
+}
